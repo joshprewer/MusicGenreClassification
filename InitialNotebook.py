929211bfef677fc00dbb054abfff27391a1dbc56
Josh Prewer <josh.prewer@focusrite.com>
2018-12-22 15:45:01 +0000

    Initial model

--- InitialNotebook.py
+++ InitialNotebook.py
@@ -1,1 +1,59 @@
-test
+# feature extractoring and preprocessing data
+import librosa
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pathlib
+import csv
+import tensorflow as tf
+from tensorflow import keras
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+cmap = plt.get_cmap('inferno')
+
+plt.figure(figsize=(10,10))
+genres = 'blues classical country disco hiphop jazz metal pop reggae rock'.split()
+
+header = 'filename chroma_stft rmse spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
+for i in range(1, 21):
+    header += f' mfcc{i}'
+header += ' label'
+header = header.split()
+
+data = pd.read_csv('data.csv')
+data.head()
+
+# Dropping unneccesary columns
+data = data.drop(['filename'],axis=1)
+
+genre_list = data.iloc[:, -1]
+encoder = LabelEncoder()
+y = encoder.fit_transform(genre_list)
+
+scaler = StandardScaler()
+X = scaler.fit_transform(np.array(data.iloc[:, 6:-1], dtype = float))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+print(X_train[0])
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Dense(256, activation='relu', input_shape=(X_train.shape[1],)))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(64, activation='relu'))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(X_train,
+                    y_train,
+                    epochs=100,
+                    batch_size=128)
+
+test_loss, test_acc = model.evaluate(X_test,y_test)
+print('test_acc: ',test_acc)
+
