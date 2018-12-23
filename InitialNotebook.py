6e7ccd7ce0a95ca456cffcc15ef479560e3951a0
Josh Prewer <josh.prewer@focusrite.com>
2018-12-23 18:51:36 +0000

    DNN implementation with confusion matrix

--- InitialNotebook.py
+++ InitialNotebook.py
@@ -2,15 +2,23 @@
 import librosa
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
 import os
 import pathlib
 import csv
 import tensorflow as tf
+import glob
+import math
+import os
+import seaborn as sns
 from tensorflow import keras
+from tensorflow.python.data import Dataset
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn import metrics
 
 cmap = plt.get_cmap('inferno')
 
@@ -37,23 +45,214 @@ scaler = StandardScaler()
 X = scaler.fit_transform(np.array(data.iloc[:, 6:-1], dtype = float))
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-print(X_train[0])
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Dense(256, activation='relu', input_shape=(X_train.shape[1],)))
-model.add(tf.keras.layers.Dense(128, activation='relu'))
-model.add(tf.keras.layers.Dense(64, activation='relu'))
-model.add(tf.keras.layers.Dense(10, activation='softmax'))
+training_examples = pd.DataFrame(data=X_train)
+training_targets = pd.DataFrame(data=y_train)
+validation_examples =  pd.DataFrame(data=X_test)
+validation_targets = pd.DataFrame(data=y_test)
 
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+def create_training_input_fn(features, labels, batch_size, num_epochs=None, shuffle=True):
+  """A custom input_fn for sending MNIST data to the estimator for training.
 
-history = model.fit(X_train,
-                    y_train,
-                    epochs=100,
-                    batch_size=128)
+  Args:
+    features: The training features.
+    labels: The training labels.
+    batch_size: Batch size to use during training.
 
-test_loss, test_acc = model.evaluate(X_test,y_test)
-print('test_acc: ',test_acc)
+  Returns:
+    A function that returns batches of training features and labels during
+    training.
+  """
+  def _input_fn(num_epochs=None, shuffle=True):
+    # Input pipelines are reset with each call to .train(). To ensure model
+    # gets a good sampling of data, even when number of steps is small, we 
+    # shuffle all the data before creating the Dataset object
+    idx = np.random.permutation(features.index)
+    raw_features = {"mfccs":features.reindex(idx)}
+    raw_targets = np.array(labels.reindex(idx).values)
+   
+    ds = Dataset.from_tensor_slices((raw_features,raw_targets)) # warning: 2GB limit
+    ds = ds.batch(batch_size).repeat(num_epochs)
+    
+    if shuffle:
+      ds = ds.shuffle(10000)
+    
+    # Return the next batch of data.
+    feature_batch, label_batch = ds.make_one_shot_iterator().get_next()
+    return feature_batch, label_batch
+
+  return _input_fn
+
+def create_predict_input_fn(features, labels, batch_size):
+  """A custom input_fn for sending mnist data to the estimator for predictions.
+
+  Args:
+    features: The features to base predictions on.
+    labels: The labels of the prediction examples.
+
+  Returns:
+    A function that returns features and labels for predictions.
+  """
+  def _input_fn():
+    raw_features = {"mfccs": features.values}
+    raw_targets = np.array(labels)
+    
+    ds = Dataset.from_tensor_slices((raw_features, raw_targets)) # warning: 2GB limit
+    ds = ds.batch(batch_size)
+    
+        
+    # Return the next batch of data.
+    feature_batch, label_batch = ds.make_one_shot_iterator().get_next()
+    return feature_batch, label_batch
+
+  return _input_fn
+
+def train_nn_classification_model(
+    learning_rate,
+    steps,
+    batch_size,
+    hidden_units,
+    training_examples,
+    training_targets,
+    validation_examples,
+    validation_targets):
+  """Trains a neural network classification model for the MNIST digits dataset.
+  
+  In addition to training, this function also prints training progress information,
+  a plot of the training and validation loss over time, as well as a confusion
+  matrix.
+  
+  Args:
+    learning_rate: An `int`, the learning rate to use.
+    steps: A non-zero `int`, the total number of training steps. A training step
+      consists of a forward and backward pass using a single batch.
+    batch_size: A non-zero `int`, the batch size.
+    hidden_units: A `list` of int values, specifying the number of neurons in each layer.
+    training_examples: A `DataFrame` containing the training features.
+    training_targets: A `DataFrame` containing the training labels.
+    validation_examples: A `DataFrame` containing the validation features.
+    validation_targets: A `DataFrame` containing the validation labels.
+      
+  Returns:
+    The trained `DNNClassifier` object.
+  """
+
+  periods = 10
+  # Caution: input pipelines are reset with each call to train. 
+  # If the number of steps is small, your model may never see most of the data.  
+  # So with multiple `.train` calls like this you may want to control the length 
+  # of training with num_epochs passed to the input_fn. Or, you can do a really-big shuffle, 
+  # or since it's in-memory data, shuffle all the data in the `input_fn`.
+  steps_per_period = steps / periods  
+  # Create the input functions.
+  predict_training_input_fn = create_predict_input_fn(
+    training_examples, training_targets, batch_size)
+  predict_validation_input_fn = create_predict_input_fn(
+    validation_examples, validation_targets, batch_size)
+  training_input_fn = create_training_input_fn(
+    training_examples, training_targets, batch_size)
+  
+  # Create the input functions.
+  predict_training_input_fn = create_predict_input_fn(
+    training_examples, training_targets, batch_size)
+  predict_validation_input_fn = create_predict_input_fn(
+    validation_examples, validation_targets, batch_size)
+  training_input_fn = create_training_input_fn(
+    training_examples, training_targets, batch_size)
+
+  
+  # Create feature columns.
+  feature_columns = [tf.feature_column.numeric_column('mfccs', shape=20)]
+
+  # Create a DNNClassifier object.
+  my_optimizer = tf.train.AdagradOptimizer(learning_rate=learning_rate)
+  my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+  classifier = tf.estimator.DNNClassifier(
+      feature_columns=feature_columns,
+      n_classes=10,
+      hidden_units=hidden_units,
+      optimizer=my_optimizer,
+      config=tf.estimator.RunConfig(keep_checkpoint_max=1)
+  )
+
+  # Train the model, but do so inside a loop so that we can periodically assess
+  # loss metrics.
+  print("Training model...")
+  print("LogLoss error (on validation data):")
+  training_errors = []
+  validation_errors = []
+  for period in range (0, periods):
+    # Train the model, starting from the prior state.
+    classifier.train(
+        input_fn=training_input_fn,
+        steps=steps_per_period
+    )
+  
+    # Take a break and compute probabilities.
+    training_predictions = list(classifier.predict(input_fn=predict_training_input_fn))
+    training_probabilities = np.array([item['probabilities'] for item in training_predictions])
+    training_pred_class_id = np.array([item['class_ids'][0] for item in training_predictions])
+    training_pred_one_hot = tf.keras.utils.to_categorical(training_pred_class_id,10)
+        
+    validation_predictions = list(classifier.predict(input_fn=predict_validation_input_fn))
+    validation_probabilities = np.array([item['probabilities'] for item in validation_predictions])    
+    validation_pred_class_id = np.array([item['class_ids'][0] for item in validation_predictions])
+    validation_pred_one_hot = tf.keras.utils.to_categorical(validation_pred_class_id,10)    
+    
+    # Compute training and validation errors.
+    training_log_loss = metrics.log_loss(training_targets, training_pred_one_hot)
+    validation_log_loss = metrics.log_loss(validation_targets, validation_pred_one_hot)
+    # Occasionally print the current loss.
+    print("  period %02d : %0.2f" % (period, validation_log_loss))
+    # Add the loss metrics from this period to our list.
+    training_errors.append(training_log_loss)
+    validation_errors.append(validation_log_loss)
+  print("Model training finished.")
+  # Remove event files to save disk space.
+  _ = map(os.remove, glob.glob(os.path.join(classifier.model_dir, 'events.out.tfevents*')))
+  
+  # Calculate final predictions (not probabilities, as above).
+  final_predictions = classifier.predict(input_fn=predict_validation_input_fn)
+  final_predictions = np.array([item['class_ids'][0] for item in final_predictions])
+  
+  
+  accuracy = metrics.accuracy_score(validation_targets, final_predictions)
+  print("Final accuracy (on validation data): %0.2f" % accuracy)
+
+  # Output a graph of loss metrics over periods.
+  plt.ylabel("LogLoss")
+  plt.xlabel("Periods")
+  plt.title("LogLoss vs. Periods")
+  plt.plot(training_errors, label="training")
+  plt.plot(validation_errors, label="validation")
+  plt.legend()
+  plt.show()
+  
+  # Output a plot of the confusion matrix.
+  cm = metrics.confusion_matrix(validation_targets, final_predictions)
+  # Normalize the confusion matrix by row (i.e by the number of samples
+  # in each class).
+  cm_normalized = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]
+  ax = sns.heatmap(cm_normalized, cmap="bone_r")
+  ax.set_aspect(1)
+  plt.title("Confusion matrix")
+  plt.xticks(np.arange(10), genres, rotation=45)
+  plt.yticks(np.arange(10), genres)
+  plt.ylabel("True label")
+  plt.xlabel("Predicted label")
+  for tick in ax.yaxis.get_minor_ticks():
+    tick.label1.set_horizontalalignment('center')
+  plt.show()
+
+  return classifier
+
+classifier = train_nn_classification_model(
+    learning_rate=0.05,
+    steps=500,
+    batch_size=30,
+    hidden_units=[100, 100],
+    training_examples=training_examples,
+    training_targets=training_targets,
+    validation_examples=validation_examples,
+    validation_targets=validation_targets)
 
