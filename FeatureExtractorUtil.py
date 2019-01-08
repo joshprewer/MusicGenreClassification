ed1b3baa4453d5a8aff824cb09ed12c16a9936cb
Josh Prewer <josh.prewer@focusrite.com>
2019-01-08 10:12:35 +0000

    Reset feature extractor to original

--- FeatureExtractorUtil.py
+++ FeatureExtractorUtil.py
@@ -5,17 +5,18 @@ import pathlib
 import csv
 import os
 
-header = 'filename chroma_stft chroma_stftV rmse rmseV spectral_centroid spectral_centroidV spectral_bandwidth spectral_bandwidthV rolloff rolloffV zero_crossing_rate zero_crossing_rateV'
+header = 'filename chroma_stft rmse spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
 for i in range(1, 21):
-    header += f' mfcc{i} mfcc{i}V'
+    header += f' mfcc{i}'
 header += ' label'
 header = header.split()
 
-file = open('newData.csv', 'w', newline='')
+file = open('data.csv', 'w', newline='')
 with file:
     writer = csv.writer(file)
     writer.writerow(header)
 genres = 'blues classical country disco hiphop jazz metal pop reggae rock'.split()
+
 for g in genres:
     for filename in os.listdir(f'./MIR/genres/{g}'):
         songname = f'./MIR/genres/{g}/{filename}'
@@ -27,9 +28,9 @@ for g in genres:
         rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
         zcr = librosa.feature.zero_crossing_rate(y)
         mfcc = librosa.feature.mfcc(y=y, sr=sr)
-        to_append = f'{filename} {np.mean(chroma_stft)} {np.std(chroma_stft)} {np.mean(rmse)} {np.std(rmse)} {np.mean(spec_cent)} {np.std(spec_cent)} {np.mean(spec_bw)} {np.std(spec_bw)} {np.mean(rolloff)}  {np.std(rolloff)} {np.mean(zcr)}  {np.std(zcr)}'    
+        to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'    
         for e in mfcc:
-            to_append += f' {np.mean(e)} {np.std(e)}'
+            to_append += f' {np.mean(e)}'
         to_append += f' {g}'
         file = open('data.csv', 'a', newline='')
         with file:
