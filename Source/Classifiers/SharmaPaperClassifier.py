81fd2b8d66615c83a3c0091937164f49a2740b72
Josh Prewer <josh.prewer@focusrite.com>
2019-02-13 11:14:38 +0000

    Remove unused classifiers

--- Source/Classifiers/SharmaPaperClassifier.py
+++ Source/Classifiers/SharmaPaperClassifier.py
@@ -3,19 +3,16 @@ import itertools
 from sklearn import metrics, svm, model_selection, utils, pipeline, decomposition, multiclass, ensemble
 from sklearn.feature_selection import SelectPercentile, chi2
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-from matplotlib import cm, gridspec, pyplot as plt
 from utilities import plot_confusion_matrix, import_data_from
 
-paperX, y, genres = import_data_from('SharmaFeaturesGTZAN.csv')
+paperX, y, genres = import_data_from('Datasets/GTZAN/SharmaFeaturesGTZAN.csv')
 
-# classifier = svm.SVC(C=15, kernel='rbf', gamma=0.001, tol=1e-2, decision_function_shape='ovo')
-# classifier = svm.SVC(C=100, kernel='poly', degree=1, gamma=0.00008, tol=1e-3, decision_function_shape='ovo')
-classifier = svm.LinearSVC(C=0.05, max_iter=10000)
+classifier = svm.LinearSVC(C=0.012, max_iter=10000)
 # ecoc = multiclass.OutputCodeClassifier(classifier, code_size=4)
 clf = multiclass.OneVsOneClassifier(classifier)
 # X = relief.transform(paperX, y, classifier)
 
-cv_results = model_selection.cross_val_score(clf, paperX, y, cv=)
+cv_results = model_selection.cross_val_score(clf, paperX, y, cv=10)
 msg = "Accuracy: %f (%f)" % (cv_results.mean(), cv_results.std())
 print(msg)
 
--- Source/Feature Extractors/MMFCDifferenceFeatures.py
+++ Source/Feature Extractors/MMFCDifferenceFeatures.py
@@ -4,12 +4,13 @@ import numpy as np
 import pathlib
 import csv
 import os
+from scipy.stats import kurtosis, skew
 
-fileName = 'MFCCDiff.csv'
+fileName = 'ReliefFSFSMeanFeaturesGTZAN.csv'
 
 header = 'filename' 
-for i in range(1, 12):
-    header += f' mfcc{i} mfcc{i}V mfcc_diff{i} mfcc_diff{i}V'
+for i in range(0, 12):
+    header += f' mfcc{i} mfcc{i}_v mfcc{i}_diff mfcc{i}_diff_v'
 header += ' label'
 header = header.split()
 
@@ -20,17 +21,15 @@ with file:
 genres = 'blues classical country disco hiphop jazz metal pop reggae rock'.split()
 
 for g in genres:
-    for filename in os.listdir(f'./MIR/GTZAN/audio/{g}'):
-        songname = f'./MIR/GTZAN/audio/{g}/{filename}'
+    for filename in os.listdir(f'./Datasets/GTZAN/audio/{g}'):
+        songname = f'./Datasets/GTZAN/audio/{g}/{filename}'
         y, sr = librosa.load(songname, mono=True, duration=30)
-        mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=12)
-        mfcc_difference = np.diff(mfcc)
+        mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=12, n_fft=512, hop_length=256)
         to_append = f'{filename}'
         for index in range(mfcc.shape[0]):
-            to_append += f' {np.mean(mfcc[index])} {np.std(mfcc[index])} {np.mean(mfcc_difference[index])} {np.std(mfcc_difference[index])}'
+            to_append += f' {np.mean(mfcc[index])} {np.std(mfcc[index])} {np.mean(np.diff(mfcc[index]))} {np.std(np.diff(mfcc[index]))}'
         to_append += f' {g}'
         file = open(fileName, 'a', newline='')
         with file:
             writer = csv.writer(file)
             writer.writerow(to_append.split())
-
