cd941fd3accb9a1de70ca61ce742b4a7075bfa43
Josh Prewer <josh.prewer@focusrite.com>
2019-02-27 11:08:08 +0000

    Custom OVO class to allow for ovo feature selection

---
+++ Source/Classifiers/ovo_feature_selection.py
@@ -0,0 +1,302 @@
+import array
+import numpy as np
+import warnings
+import scipy.sparse as sp
+import itertools
+
+from sklearn.base import BaseEstimator, ClassifierMixin, clone, is_classifier
+from sklearn.base import MetaEstimatorMixin, is_regressor
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.utils import check_random_state
+from sklearn.utils.validation import _num_samples
+from sklearn.utils.validation import check_is_fitted
+from sklearn.utils.validation import check_X_y, check_array
+from sklearn.utils.multiclass import (_check_partial_fit_first_call,
+                               check_classification_targets,
+                               _ovr_decision_function)
+from sklearn.utils.metaestimators import _safe_split, if_delegate_has_method
+
+from sklearn.utils._joblib import Parallel
+from sklearn.utils._joblib import delayed
+from sklearn.externals.six.moves import zip as izip
+from feature_selectors import harmony_run, HarmonyCore
+
+__all__ = [
+    "OneVsOneClassifier",
+]
+
+
+def _fit_binary(estimator, X, y, classes=None):
+    """Fit a single binary estimator."""
+    unique_y = np.unique(y)
+
+    obj = harmony_run.objective_function(X, y, hmcr_proba=0.99, iteration=10000)
+    hc = HarmonyCore.HarmonyCore(obj)
+    hms, hms_scores, max_score_index = hc.run()
+
+    feature_sets = np.nonzero(hms[max_score_index])[0]
+    X = np.take(X, feature_sets, axis=1)
+
+    if len(unique_y) == 1:
+        if classes is not None:
+            if y[0] == -1:
+                c = 0
+            else:
+                c = y[0]
+            warnings.warn("Label %s is present in all training examples." %
+                          str(classes[c]))
+        estimator = _ConstantPredictor().fit(X, unique_y)
+    else:
+        estimator = clone(estimator)
+        estimator.fit(X, y)
+    return estimator, feature_sets
+
+
+def _partial_fit_binary(estimator, X, y):
+    """Partially fit a single binary estimator."""
+    estimator.partial_fit(X, y, np.array((0, 1)))
+    return estimator
+
+
+def _predict_binary(estimator, X):
+    """Make predictions using a single binary estimator."""
+    if is_regressor(estimator):
+        return estimator.predict(X)
+    try:
+        score = np.ravel(estimator.decision_function(X))
+    except (AttributeError, NotImplementedError):
+        # probabilities of the positive class
+        score = estimator.predict_proba(X)[:, 1]
+    return score
+
+
+def _check_estimator(estimator):
+    """Make sure that an estimator implements the necessary methods."""
+    if (not hasattr(estimator, "decision_function") and
+            not hasattr(estimator, "predict_proba")):
+        raise ValueError("The base estimator should implement "
+                         "decision_function or predict_proba!")
+
+
+class _ConstantPredictor(BaseEstimator):
+
+    def fit(self, X, y):
+        self.y_ = y
+        return self
+
+    def predict(self, X):
+        check_is_fitted(self, 'y_')
+
+        return np.repeat(self.y_, X.shape[0])
+
+    def decision_function(self, X):
+        check_is_fitted(self, 'y_')
+
+        return np.repeat(self.y_, X.shape[0])
+
+    def predict_proba(self, X):
+        check_is_fitted(self, 'y_')
+
+        return np.repeat([np.hstack([1 - self.y_, self.y_])],
+                         X.shape[0], axis=0)
+
+
+
+def _fit_ovo_binary(estimator, X, y, i, j):
+    """Fit a single binary estimator (one-vs-one)."""
+    cond = np.logical_or(y == i, y == j)
+    y = y[cond]
+    y_binary = np.empty(y.shape, np.int)
+    y_binary[y == i] = 0
+    y_binary[y == j] = 1
+    indcond = np.arange(X.shape[0])[cond]
+    return _fit_binary(estimator,
+                       _safe_split(estimator, X, None, indices=indcond)[0],
+                       y_binary, classes=[i, j]), indcond
+
+
+def _partial_fit_ovo_binary(estimator, X, y, i, j):
+    """Partially fit a single binary estimator(one-vs-one)."""
+
+    cond = np.logical_or(y == i, y == j)
+    y = y[cond]
+    if len(y) != 0:
+        y_binary = np.zeros_like(y)
+        y_binary[y == j] = 1
+        return _partial_fit_binary(estimator, X[cond], y_binary)
+    return estimator
+
+
+class OneVsOneClassifier(BaseEstimator, ClassifierMixin, MetaEstimatorMixin):
+    """One-vs-one multiclass strategy
+    This strategy consists in fitting one classifier per class pair.
+    At prediction time, the class which received the most votes is selected.
+    Since it requires to fit `n_classes * (n_classes - 1) / 2` classifiers,
+    this method is usually slower than one-vs-the-rest, due to its
+    O(n_classes^2) complexity. However, this method may be advantageous for
+    algorithms such as kernel algorithms which don't scale well with
+    `n_samples`. This is because each individual learning problem only involves
+    a small subset of the data whereas, with one-vs-the-rest, the complete
+    dataset is used `n_classes` times.
+    Read more in the :ref:`User Guide <ovo_classification>`.
+    Parameters
+    ----------
+    estimator : estimator object
+        An estimator object implementing `fit` and one of `decision_function`
+        or `predict_proba`.
+    n_jobs : int or None, optional (default=None)
+        The number of jobs to use for the computation.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+        for more details.
+    Attributes
+    ----------
+    estimators_ : list of `n_classes * (n_classes - 1) / 2` estimators
+        Estimators used for predictions.
+    classes_ : numpy array of shape [n_classes]
+        Array containing labels.
+    """
+
+    def __init__(self, estimator, n_jobs=None):
+        self.estimator = estimator
+        self.n_jobs = n_jobs
+
+    def fit(self, X, y):
+        """Fit underlying estimators.
+        Parameters
+        ----------
+        X : (sparse) array-like, shape = [n_samples, n_features]
+            Data.
+        y : array-like, shape = [n_samples]
+            Multi-class targets.
+        Returns
+        -------
+        self
+        """
+        X, y = check_X_y(X, y, accept_sparse=['csr', 'csc'])
+        check_classification_targets(y)
+
+        self.classes_ = np.unique(y)
+        if len(self.classes_) == 1:
+            raise ValueError("OneVsOneClassifier can not be fit when only one"
+                             " class is present.")
+        n_classes = self.classes_.shape[0]
+        estimators_indices = list(zip(*(Parallel(n_jobs=self.n_jobs)(
+            delayed(_fit_ovo_binary)
+            (self.estimator, X, y, self.classes_[i], self.classes_[j])
+            for i in range(n_classes) for j in range(i + 1, n_classes)))))
+
+        self.estimators_ = estimators_indices[0]
+        try:
+            self.pairwise_indices_ = (
+                estimators_indices[1] if self._pairwise else None)
+        except AttributeError:
+            self.pairwise_indices_ = None
+
+        return self
+
+    @if_delegate_has_method(delegate='estimator')
+    def partial_fit(self, X, y, classes=None):
+        """Partially fit underlying estimators
+        Should be used when memory is inefficient to train all data. Chunks
+        of data can be passed in several iteration, where the first call
+        should have an array of all target variables.
+        Parameters
+        ----------
+        X : (sparse) array-like, shape = [n_samples, n_features]
+            Data.
+        y : array-like, shape = [n_samples]
+            Multi-class targets.
+        classes : array, shape (n_classes, )
+            Classes across all calls to partial_fit.
+            Can be obtained via `np.unique(y_all)`, where y_all is the
+            target vector of the entire dataset.
+            This argument is only required in the first call of partial_fit
+            and can be omitted in the subsequent calls.
+        Returns
+        -------
+        self
+        """
+        if _check_partial_fit_first_call(self, classes):
+            self.estimators_ = [clone(self.estimator) for i in
+                                range(self.n_classes_ *
+                                      (self.n_classes_ - 1) // 2)]
+
+        if len(np.setdiff1d(y, self.classes_)):
+            raise ValueError("Mini-batch contains {0} while it "
+                             "must be subset of {1}".format(np.unique(y),
+                                                            self.classes_))
+
+        X, y = check_X_y(X, y, accept_sparse=['csr', 'csc'])
+        check_classification_targets(y)
+        combinations = itertools.combinations(range(self.n_classes_), 2)
+        self.estimators_ = Parallel(
+            n_jobs=self.n_jobs)(
+                delayed(_partial_fit_ovo_binary)(
+                    estimator, X, y, self.classes_[i], self.classes_[j])
+                for estimator, (i, j) in izip(self.estimators_,
+                                              (combinations)))
+
+        self.pairwise_indices_ = None
+
+        return self
+
+    def predict(self, X):
+        """Estimate the best class label for each sample in X.
+        This is implemented as ``argmax(decision_function(X), axis=1)`` which
+        will return the label of the class with most votes by estimators
+        predicting the outcome of a decision for each possible class pair.
+        Parameters
+        ----------
+        X : (sparse) array-like, shape = [n_samples, n_features]
+            Data.
+        Returns
+        -------
+        y : numpy array of shape [n_samples]
+            Predicted multi-class targets.
+        """
+        Y = self.decision_function(X)
+        if self.n_classes_ == 2:
+            return self.classes_[(Y > 0).astype(np.int)]
+        return self.classes_[Y.argmax(axis=1)]
+
+    def decision_function(self, X):
+        """Decision function for the OneVsOneClassifier.
+        The decision values for the samples are computed by adding the
+        normalized sum of pair-wise classification confidence levels to the
+        votes in order to disambiguate between the decision values when the
+        votes for all the classes are equal leading to a tie.
+        Parameters
+        ----------
+        X : array-like, shape = [n_samples, n_features]
+        Returns
+        -------
+        Y : array-like, shape = [n_samples, n_classes]
+        """
+        check_is_fitted(self, 'estimators_')
+
+        indices = self.pairwise_indices_
+        if indices is None:
+            Xs = [X] * len(self.estimators_)
+        else:
+            Xs = [X[:, idx] for idx in indices]
+
+        predictions = np.vstack([est.predict(np.take(X, Xi, axis=1))
+                                 for est, Xi in self.estimators_]).T
+        confidences = np.vstack([_predict_binary(est, np.take(X, Xi, axis=1))
+                                 for est, Xi in self.estimators_]).T
+        Y = _ovr_decision_function(predictions,
+                                   confidences, len(self.classes_))
+        if self.n_classes_ == 2:
+            return Y[:, 1]
+        return Y
+
+    @property
+    def n_classes_(self):
+        return len(self.classes_)
+
+    @property
+    def _pairwise(self):
+        """Indicate if wrapped estimator is using a precomputed Gram matrix"""
+        return getattr(self.estimator, "_pairwise", False)
