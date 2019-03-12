6e17c8650216aa5fd1a4f02033ee0e23cc729d3c
Josh Prewer <josh.prewer@focusrite.com>
2019-03-12 18:22:30 +0000

    Remove unused files

--- Source/Classifiers/SharmaPaperClassifier.py
+++
@@ -1,9 +0,0 @@
-from sklearn import metrics, svm, model_selection, multiclass, ensemble
-from utilities import plot_confusion_matrix, import_data_from, cross_validation
-
-X, y, genres = import_data_from('Datasets/GTZAN/SharmaFeaturesGTZAN.csv')
-
-classifier = svm.LinearSVC(C=0.012, max_iter=10000)
-clf = multiclass.OneVsOneClassifier(classifier)
-
-cross_validation(X, y, clf, genres)
